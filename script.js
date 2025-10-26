17b20c0a3cf969e3dfd8aa9300da0f2d5e1a966d
sayyadshahi8252 <167566299+sayyadshahi8252@users.noreply.github.com>
2025-10-26 16:34:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+function formsubmit(event){
+	event.preventDefault();
+	const fname=document.getElementById("firstname").value
+	const lname=document.getElementById("lastname").value
+	const upnumber=document.getElementById("PhoneNumber").value
+	const uemail=document.getElementById("Email").value
+	alert(` First Name: ${fname} Last Name: ${lname} Phone Number: ${upnumber} Email ID: ${uemail}`)
+}
